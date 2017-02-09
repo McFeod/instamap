85606b9724f2ec31aef59a93e43814c0947fd499
McFeod <mcfeod@gmail.com>
2017-02-09 17:24:01 +0500

    fixed issue with equal coordinates

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,26 @@
 var loaded = 0;
 
+MarkerClusterer.prototype.onClick = function() {
+    return true;
+};
+
+
+ClusterIcon.prototype.triggerClusterClick = function(event) {
+    var markerClusterer = this.cluster_.getMarkerClusterer();
+    google.maps.event.trigger(markerClusterer, 'clusterclick', this.cluster_, event);
+
+    var zoom = this.map_.getZoom();
+    var maxZoom = markerClusterer.getMaxZoom();
+
+    if (markerClusterer.isZoomOnClick()) {
+        this.map_.fitBounds(this.cluster_.getBounds());
+    }
+
+    if (zoom >= maxZoom && this.cluster_.markers_ && this.cluster_.markers_.length > 1) {
+        return showAllPreviews(markerClusterer);
+    }
+};
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 2,
@@ -31,15 +52,38 @@ function addMarker(map, pic, cluster){
             map: map,
             icon: {
                 url: pic.preview,
-                scaledSize: new google.maps.Size(32, 32),
+                scaledSize: new google.maps.Size(32, 32)
             }
         });
+    marker.picture_link = pic.image;
     cluster.addMarker(marker);
     marker.addListener('click', function () {
-        $('#modal_body').html('<img src="' + pic.image + '" alt="">');
-        $('#pic_modal').modal('show');
+        showPicture(pic.image);
     });
     loaded++;
     $('#footer').html(loaded + 'of ' + $('#count').html())
 
+}
+
+function showAllPreviews(mc) {
+    var cluster = mc.clusters_;
+     if (cluster.length == 1 && cluster[0].markers_ && cluster[0].markers_.length > 1){
+          var markers = cluster[0].markers_;
+          var output = $('#modal_body');
+          output.html('');
+          for (var i=0; i < markers.length; i++)  {
+              output.append('<img src="' + markers[i].icon.url +
+                  '" onclick="showPicture(\'' + markers[i].picture_link +'\')"/>');
+              $('#pic_modal').modal('show');
+          }
+
+          return false;
+     }
+
+     return true;
+}
+
+function showPicture(link) {
+    $('#modal_body').html('<img src="' + link + '" alt="">');
+    $('#pic_modal').modal('show');
 }
